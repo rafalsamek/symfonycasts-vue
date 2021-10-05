ecb15a66660f0146557b53c38b258f1199f43d98
Rafał Samek <rafal.samek@gmail.com>
2021-10-05 14:12:12 +0200

    Chapter 03 - Vue Instance & Dynamic Data

--- assets/js/products.js
+++ assets/js/products.js
@@ -8,6 +8,9 @@ const app = new Vue({
         };
     },
     template: '<h1>Hello {{ firstName }}! Is this cooler?</h1>',
+    render(h) {
+        return Vue.compile(this.$options.template).render.call(this, h);
+    },
 });
 
 window.app = app;
