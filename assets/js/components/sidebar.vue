c0e32fcc796b20e640ea94991c713317a5a59b4d
Rafał Samek <rafal.samek@gmail.com>
2021-10-06 20:28:16 +0200

    Chapter 23 - Using Shared CSS

--- assets/js/components/sidebar.vue
+++ assets/js/components/sidebar.vue
@@ -1,5 +1,5 @@
 <template>
-    <div :class="componentClass">
+    <div :class="[this.$style.component, 'p-3', 'mb-5']">
         <div v-show="!collapsed">
             <h5 class="text-center">
                 Categories
@@ -60,22 +60,6 @@ export default {
             ],
         };
     },
-    computed: {
-        /**
-         * Computes the component classes depending on collapsed state
-         *
-         * @returns string[]
-         */
-        componentClass() {
-            const classes = [this.$style.component, 'p-3', 'mb-5'];
-
-            if (this.collapsed) {
-                classes.push(this.$style.collapsed);
-            }
-
-            return classes;
-        },
-    },
 };
 </script>
 
@@ -84,10 +68,6 @@ export default {
 
 .component {
     @include light-component;
-
-    &.collapsed {
-        width: 70px;
-    }
 
     ul {
         li a:hover {
