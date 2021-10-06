0e7ed8f1511ca4f2a80367b76b257d911a690be4
Rafał Samek <rafal.samek@gmail.com>
2021-10-06 18:24:20 +0200

    Chapter 19 - v-if, v-show and Conditional Classes - v-if

--- assets/js/components/sidebar.vue
+++ assets/js/components/sidebar.vue
@@ -3,32 +3,33 @@
         :class="[$style.sidebar, 'p-3', 'mb-5']"
         :style="{ width: collapsed ? '70px' : 'auto' }"
     >
-        <h5 class="text-center">
-            Categories
-        </h5>
+        <div v-if="!collapsed">
+            <h5 class="text-center">
+                Categories
+            </h5>
 
-        <ul class="nav flex-column mb4">
-            <li class="nav-item">
-                <a
-                    class="nav-link"
-                    href="/"
-                >All Products</a>
-            </li>
-            <li
-                v-for="(category, index) in categories"
-                :key="index"
-                class="nav-item"
-            >
-                <a
-                    class="nav-link"
-                    :href="category.link"
+            <ul class="nav flex-column mb4">
+                <li class="nav-item">
+                    <a
+                        class="nav-link"
+                        href="/"
+                    >All Products</a>
+                </li>
+                <li
+                    v-for="(category, index) in categories"
+                    :key="index"
+                    class="nav-item"
                 >
-                    {{ category.name }}
-                </a>
-            </li>
-        </ul>
-        <hr>
-
+                    <a
+                        class="nav-link"
+                        :href="category.link"
+                    >
+                        {{ category.name }}
+                    </a>
+                </li>
+            </ul>
+            <hr>
+        </div>
         <div class="d-flex justify-content-end">
             <button
                 class="btn btn-secondary btn-sm"
