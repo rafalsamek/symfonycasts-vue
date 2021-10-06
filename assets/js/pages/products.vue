aa236a9f379260807f5ab605ce5a3897e731a49c
Rafał Samek <rafal.samek@gmail.com>
2021-10-06 14:06:36 +0200

    Chapter 09 - v-bind: Dynamic Attributes - legend component with title prop type and required

--- assets/js/pages/products.vue
+++ assets/js/pages/products.vue
@@ -47,7 +47,7 @@
 
                 <div class="row">
                     <span class="p-3">
-                        <legend-component/>
+                        <legend-component :title="legend"/>
                     </span>
                 </div>
             </div>
