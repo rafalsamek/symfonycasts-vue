75a616a8609d65a0b2e8155e37011000364889ac
Rafał Samek <rafal.samek@gmail.com>
2021-10-07 20:54:08 +0200

    Chapter 34 - Smarter Loading: AJAX status as State - catalog loading

--- assets/js/components/sidebar.vue
+++ assets/js/components/sidebar.vue
@@ -5,6 +5,8 @@
                 Categories
             </h5>
 
+            <loading v-show="loading" />
+
             <ul class="nav flex-column mb4">
                 <li class="nav-item">
                     <a
@@ -45,9 +47,13 @@
 
 <script>
 import axios from 'axios';
+import Loading from '@/components/loading';
 
 export default {
     name: 'Sidebar',
+    components: {
+        Loading,
+    },
     props: {
         collapsed: {
             type: Boolean,
@@ -62,6 +68,11 @@ export default {
         return {
             categories: [],
         };
+    },
+    computed: {
+        loading() {
+            return this.categories.length === 0;
+        },
     },
     async created() {
         const response = await axios.get('/api/categories');
